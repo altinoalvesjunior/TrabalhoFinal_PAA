f5239a1cdce3785b2650843366f101ce2cba14b3
Altino Júnior <altinoalvesjunior@gmail.com>
2020-11-26 09:29:53 -0300

    ALT: Problema da Subsequencia finalizado

--- Projeto/TF_PAA-Lab/src/Subsequencia/Programa.java
+++ Projeto/TF_PAA-Lab/src/Subsequencia/Programa.java
@@ -11,13 +11,25 @@ public class Programa {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Digite a primeira palavra: ");
-        String palavra1 = sc.nextLine().toUpperCase();
-        System.out.print("Digite a primeira palavra: ");
-        String palavra2 = sc.nextLine().toUpperCase();
+        int op;
 
-        System.out.print("\nA subsequência é: ");
-        System.out.print("\nO tamanho da subsequência é: " + encontraSubsequencia(palavra1, palavra2) + "\n");
+        do {
+            System.out.println("\n\t------------ P6 - Subsequênia ------------");
+            System.out.print("\nDigite a primeira palavra: ");
+            String palavra1 = sc.nextLine().toUpperCase();
+            System.out.print("Digite a primeira palavra: ");
+            String palavra2 = sc.nextLine().toUpperCase();
+
+            System.out.print("\nA subsequência é: ");
+            System.out.print("\nO tamanho da subsequência é: " + encontraSubsequencia(palavra1, palavra2) + "\n");
+
+            System.out.println("---------------------------------------------------");
+            System.out.print("Deseja executar novamente? \n 1 - Sim \n 2 - Não \n\nEscolha: ");
+            op = sc.nextInt();
+            sc.nextLine();
+        } while (op != 2);
+
+        System.out.println("Saindo do programa!");
 
         sc.close();
     }
